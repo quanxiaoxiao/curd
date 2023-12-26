bdff5cac63e7cae5b61a5af4ac5720cc91a44b19
quan <quan_xiaoxiao@qq.com>
2023-12-26 16:00:45 +0800

    xxx

--- src/index.mjs
+++ src/index.mjs
@@ -1,80 +1,132 @@
+/**
+ * @param {Array<*>} list
+ * @param {*} query
+ * @returns {number}
+ */
 const findIndex = (list, query) => {
   if (!Array.isArray(list)) {
     throw new Error('data is not array');
+  }
+  if (query == null) {
+    return -1;
   }
   const size = list.length;
   if (size === 0) {
     return -1;
   }
-  if (typeof query === 'function') {
+  const type = typeof query;
+  if (type === 'function') {
     return list.findIndex(query);
   }
-  const keys = Object.keys(query);
-  if (keys.length === 0) {
-    return 0;
-  }
-  if (typeof query === 'object' && query !== null) {
+  if (query !== null && type === 'object') {
+    const keys = Object.keys(query);
+    if (keys.length === 0) {
+      return 0;
+    }
     return list.findIndex((item) => keys.every((key) => item[key] === query[key]));
   }
   return list.indexOf(query);
 };
 
+/**
+ * @template T
+ * @param {Array<T>} list
+ * @param {*} query
+ * @param {T} d
+ * @returns {Array<T>}
+ */
 export const update = (list, query, d) => {
   const index = findIndex(list, query);
   if (index === -1) {
     return list;
   }
-  return [
-    ...list.slice(0, index),
-    { ...list[index], ...d },
-    ...list.slice(index + 1),
-  ];
+  const obj = list[index];
+  if (typeof obj === 'object') {
+    const item = {
+      ...list[index],
+      ...d,
+    };
+    return list.slice(0, index)
+      .concat([item])
+      .concat(list.slice(index + 1));
+  }
+  return list.slice(0, index)
+    .concat([d])
+    .concat(list.slice(index + 1));
 };
 
+/**
+ * @template T
+ * @param {Array<T>} list
+ * @param {*} query
+ * @returns {Array<T>}
+ */
 export const remove = (list, query) => {
   const index = findIndex(list, query);
   if (index === -1) {
     return list;
   }
-  return [
-    ...list.slice(0, index),
-    ...list.slice(index + 1),
-  ];
+  return list.slice(0, index).concat(list.slice(index + 1));
 };
 
+/**
+ * @template T
+ * @param {Array<T>} list
+ * @param {T} item
+ * @param {*} query
+ * @return {Array<T>}
+ */
 export const insert = (list, item, query) => {
   if (query) {
     const index = findIndex(list, query);
     if (index === -1) {
       return list;
     }
-    const arr = [...list];
-    arr.splice(index, 0, item);
-    return arr;
+    return list.slice(0, index)
+      .concat([item])
+      .concat(list.slice(index));
   }
   return [item, ...list];
 };
 
+/**
+ * @template T
+ * @param {Array<T>} list
+ * @param {T} item
+ * @returns {Array<T>}
+ */
 export const append = (list, item) => [...list, item];
 
+/**
+ * @param {Array<*>} list
+ * @param {*} query
+ * @returns {Array<*>}
+ */
 export const filter = (list, query) => {
-  if (typeof query === 'function') {
+  if (query == null) {
+    return list;
+  }
+  const type = typeof query;
+  if (type === 'function') {
     return list.filter(query);
+  }
+  if (type !== 'object') {
+    return list.filter((item) => item !== query);
   }
   const keys = Object.keys(query);
   if (keys.length === 0) {
     return list;
   }
-  const isPlainObject = typeof query === 'object' && query !== null;
   return list
-    .filter((item) => {
-      if (isPlainObject) {
-        return !keys.every((key) => item[key] === query[key]);
-      }
-      return item !== query;
-    });
+    .filter((item) => !keys.every((key) => item[key] === query[key]));
 };
 
+/**
+ * @template T
+ * @param {Array<T>} list
+ * @param {*} query
+ * @returns {T|null}
+ */
 export const find = (list, query) => {
   const index = findIndex(list, query);
   if (index === -1) {
